0139b115d8f37bae615b2bd016de256e8dea057c
Mazlum Baran <78929600+mazlumbaran@users.noreply.github.com>
2021-02-14 14:29:16 +0300

    Add files via upload

---
+++ MusteriManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " - " + "Müşteri Eklendi! ");        
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " - " + "Müşteri Silindi! ");
+        }
+
+        public void MusteriGuncelleme(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Adi + " " + musteri.Soyadi + " - " + "Müşteri Bilgileri Güncellendi! ");
+        }
+        public void MusteriListeleme(Musteri[] musteri)
+        {
+            foreach (var item in musteri)
+            {
+                Console.WriteLine("Müşteri Numarası : " + item.Id);
+                Console.WriteLine("Müşteri Adı : " + item.Adi);
+                Console.WriteLine("Müşteri Soyadı : " + item.Soyadi);
+                Console.WriteLine("Müşteri Telefonu : " + item.MobilNo);
+                Console.WriteLine("Müşteri Risk Yüzdesi : " + "% " + item.RiskYuzdesi);
+                Console.WriteLine("-------------");
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 0001;
+            musteri1.Adi = "Hasan";
+            musteri1.Soyadi = "Gürbüz";
+            musteri1.MobilNo = 01234567890;
+            musteri1.RiskYuzdesi = 5;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 0002;
+            musteri2.Adi = "Engin";
+            musteri2.Soyadi = "Demiroğ";
+            musteri2.MobilNo = 01547567890;
+            musteri2.RiskYuzdesi = 51;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 0003;
+            musteri3.Adi = "Mazlum";
+            musteri3.Soyadi = "Baran";
+            musteri3.MobilNo = 01265417890;
+            musteri3.RiskYuzdesi = 14;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+
+            musteriManager.MusteriSil(musteri1);
+            musteriManager.MusteriSil(musteri2);
+            
+            musteriManager.MusteriGuncelleme(musteri3);
+
+            Console.WriteLine("----------MÜŞTERİ LİSTESİ------------");
+            musteriManager.MusteriListeleme(musteriler);
+        }
+    }
+}
